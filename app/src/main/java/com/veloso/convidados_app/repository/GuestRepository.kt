743177a875d71988937e1f9df4f884d0a643ba77
Mateus Veloso <56977047+mateusvzo@users.noreply.github.com>
2023-01-13 21:20:21 -0300

    added update and delete functionsy
    added new functions in guestRepository.

--- app/src/main/java/com/veloso/convidados_app/repository/GuestRepository.kt
+++ app/src/main/java/com/veloso/convidados_app/repository/GuestRepository.kt
@@ -2,6 +2,7 @@ package com.veloso.convidados_app.repository
 
 import android.content.ContentValues
 import android.content.Context
+import android.provider.ContactsContract.Data
 import com.veloso.convidados_app.constants.DataBaseConstants
 import com.veloso.convidados_app.model.GuestModel
 
@@ -38,5 +39,35 @@ class GuestRepository private constructor(context: Context) {
         }
     }
 
-    fun update() {}
+    fun update(guest: GuestModel): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            var presence = if (guest.presence) 1 else 0
+            val args = arrayOf(guest.id.toString())
+
+            val values = ContentValues()
+            values.put(DataBaseConstants.GUEST.COLUMNS.NAME, guest.name)
+            values.put(DataBaseConstants.GUEST.COLUMNS.PRESENCE, presence)
+
+            db.update(DataBaseConstants.GUEST.TABLE_NAME, values, selection, args)
+
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
+
+    fun delete(id: Int): Boolean {
+        return try {
+            val db = guestDataBase.writableDatabase
+            val selection = DataBaseConstants.GUEST.COLUMNS.ID + " = ?"
+            val args = arrayOf(id.toString())
+
+            db.delete(DataBaseConstants.GUEST.TABLE_NAME, selection, args)
+            true
+        } catch (e: Exception) {
+            false
+        }
+    }
 }
